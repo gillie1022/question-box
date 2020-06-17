351f4ae7c8591ff6eeb5796ba12247ce39a0f32a
gillie1022 <dave.gillie@icloud.com>
2020-06-17 12:00:56 -0400

    imported models to views

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-
+from .models import Question, Answer
 # Create your views here.
 def homepage(request):
     if request.user.is_authenticated:
