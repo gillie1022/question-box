c97aa09cf80226bcc872f0f7de85da11c9a99d26
gillie1022 <dave.gillie@icloud.com>
2020-06-23 19:13:53 -0400

    added answers

--- api/serializers.py
+++ api/serializers.py
@@ -5,11 +5,17 @@ from core.models import Question, Answer
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'url', 'username', 'email', 'is_staff']
+        fields = ['id', 'url', 'username', 'email', 'is_staff',]
+
+class AnswerSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField()
+    class Meta:
+        model = Answer
+        fields = ['author', 'body', 'answered_on', "marked_correct",]
 
 class QuestionSerializer(serializers.HyperlinkedModelSerializer):
+    answers = AnswerSerializer(many=True, required=False)
     user = serializers.StringRelatedField()
     class Meta:
         model = Question
-        fields = ['url', 'title', 'body', 'user']
-
+        fields = ['url','user', 'title', 'body', 'answers',]
